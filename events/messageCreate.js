9a52dc3be53122dcc5fe544ccbd75429619e74cb
tabularelf <tabularelf@gmail.com>
2023-12-15 18:23:57 +1100

    Updated secondary emote to be randomized

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,32 @@
 const { Events } = require('discord.js');
-
+const Emotes = [
+    `🍳`,
+    `🔪`,
+    `🍴`,
+    `🍔`,
+    `🍕`,
+    `🍠`,
+    `🍣`,
+    `🍤`,
+    `🍩`,
+    `🍪`,
+    `🍰`,
+    `🥗`,
+    `🍲`,
+    `🥙`,
+    `🥞`,
+    `🥓`,
+    '👍',
+    '👌',
+    '☕'
+]
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
         if (message.mentions.has(message.client.user.id) && (!message.mentions.everyone)) {
+            let emotePos = Math.floor(Math.random() * Emotes.length)
             await message.react(`👨‍🍳`);
-            await message.react(`🔪`);
+            await message.react(Emotes[emotePos]);
         }
     }
 }
