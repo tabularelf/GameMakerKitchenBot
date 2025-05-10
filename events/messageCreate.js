86f012028b98c4037c09c870f9991d852b69d705
tabularelf <tabularelf@gmail.com>
2025-05-10 23:22:26 +1000

    Moved link handling to separate function.

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, MessageFlags } = require('discord.js');
 const linkHandle = require('../src/link-handling-gist.js');
 const Emotes = [
     `🍳`,
@@ -21,64 +21,62 @@ module.exports = {
             await message.react(`👨‍🍳`);
             await message.react(Emotes[emotePos]);
         } 
+        
+        await handleLinkSummary(message);
+    }
+}
 
-        var msg = message.content;
-    
-        const githubMatch = [...msg.matchAll(
-            /https?:\/\/github\.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(.+?)\/(.+?)#L(\d+)[-~]?L?(\d*)/g
-        )];
-
-        const gitlabMatch = [...msg.matchAll(
-            /https?:\/\/gitlab\.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/-\/blob\/(.+?)\/(.+?)#L(\d+)-?(\d*)/g
-        )];
-
-        const gistMatch = [...msg.matchAll(
-            /https?:\/\/gist\.github\.com\/([a-zA-Z0-9-_]+\/[0-9a-zA-Z]+)\/?([0-9a-z]*)\/*#file-(.+?)-L(\d+)[-~]?L?(\d*)/g
-        )];
-
-        if (githubMatch.length > 0 || gitlabMatch.length > 0 || gistMatch.length > 0) {
-            let results = [];
-            let result;
-            if (githubMatch.length > 0) {
-                for(match of githubMatch) {
-                    result = await linkHandle(match, "Github");
-                    results.push(result);
-                }
+async function handleLinkSummary(message) {
+    var msg = message.content;
+    const githubMatch = [...msg.matchAll(
+        /https?:\/\/github\.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/blob\/(.+?)\/(.+?)#L(\d+)[-~]?L?(\d*)/g
+    )];
+    const gitlabMatch = [...msg.matchAll(
+        /https?:\/\/gitlab\.com\/([a-zA-Z0-9-_]+\/[A-Za-z0-9_.-]+)\/-\/blob\/(.+?)\/(.+?)#L(\d+)-?(\d*)/g
+    )];
+    const gistMatch = [...msg.matchAll(
+        /https?:\/\/gist\.github\.com\/([a-zA-Z0-9-_]+\/[0-9a-zA-Z]+)\/?([0-9a-z]*)\/*#file-(.+?)-L(\d+)[-~]?L?(\d*)/g
+    )];
+    if (githubMatch.length > 0 || gitlabMatch.length > 0 || gistMatch.length > 0) {
+        var results = [];
+        let result;
+        if (githubMatch.length > 0) {
+            for(match of githubMatch) {
+                result = await linkHandle(match, "Github");
+                results.push(result);
             }
+        }
+        if (gitlabMatch.length > 0) {
+            for(match of gitlabMatch) {
+                result = await linkHandle(match, "Gitlab");
+                results.push(result);
+            }
+        }
+        if (gistMatch.length > 0) {
+            for(match of gistMatch) {
+                result = await linkHandle(match, "Gist");
+                results.push(result);
+            }
+        }
+        
+        // Handle the actual response
+        let response = "";
+        for(resultMatch of results) {
+            if (resultMatch == null) {
+                // Do nothing;
+                continue;
+            }
+            response += "```" + resultMatch.extension + `\n${resultMatch.toDisplay}\n` + "```\n";
+        }
+        var length = response.length;
 
-            if (gitlabMatch.length > 0) {
-                for(match of gitlabMatch) {
-                    result = await linkHandle(match, "Gitlab");
-                    results.push(result);
-                }
-            }
-
-            if (gistMatch.length > 0) {
-                for(match of gistMatch) {
-                    result = await linkHandle(match, "Gist");
-                    results.push(result);
-                }
-            }
-            
-            // Handle the actual response
-            let response = "";
-            for(resultMatch of results) {
-                // await message.channel.send
-                if (resultMatch == null) {
-                    // Do nothing;
-                    continue;
-                }
-                response += "```" + resultMatch.extension + `\n${resultMatch.toDisplay}\n` + "```\n";
-            }
-
-            if (response.length > 2000) {
-                response = `The associated link contents was too long. Got ${response.length}, expected 2000 or less.`;
-            } if (response.length == 0) {
-                return;
-            }
-            
-            await message.reply({content: response, allowedMentions: {repliedUser: false}});
+        if (length > 2000) {
+            return await message.reply({content: `The associated link contents was too long. Got ${response.length}, expected 2000 or less.`, allowedMentions: {repliedUser: false}});
+        } if (length == 0) {
             return;
         }
+        
+        await message.reply({content: response, allowedMentions: {repliedUser: false}});
+        return;
     }
 }
