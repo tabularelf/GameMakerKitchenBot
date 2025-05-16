124cbed49817c8c82e3a66b948777e24dacfb7cc
tabularelf <tabularelf@gmail.com>
2025-05-16 22:22:28 +1000

    Update contribution.js

--- commands/contribution.js
+++ commands/contribution.js
@@ -19,7 +19,7 @@ module.exports = {
         var msg = ""
         var files = [];
         if (interaction.options.getSubcommand() === 'input') {
-           msg = "Please contribute your edits or suggestions directly using GitHub: <https://github.com/offalynne/Input/tree/docs/docs>";
+           msg = "Please contribute your edits or suggestions directly using GitHub\n<https://github.com/offalynne/Input/tree/docs/docs>";
            files.push(new AttachmentBuilder('./assets/contribution.png', {name: 'contribution.png'}));
         } else {
             msg = "Please follow along with the contribution guidelines of the specified resource. If none are specified, please ask the author for the contribution guidelines.";
