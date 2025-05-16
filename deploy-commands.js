e7d756cd15cfd08b86af303a763ded73bea8d5f8
tabularelf <tabularelf@gmail.com>
2025-05-17 03:31:10 +1000

    Added nullish

--- deploy-commands.js
+++ deploy-commands.js
@@ -15,7 +15,7 @@ module.exports = function(client, global = false) {
 		const filePath = path.join(commandsPath, file);
 		fs.watchFile(require("path").resolve(filePath), () => { delete require.cache[require.resolve(filePath)] });
 		const command = require(filePath);
-		if (command.private == false) {
+		if (command.private ?? false) {
 			commands.push(command.data.toJSON());
 		}
 		commandsPrivate.push(command.data.toJSON());
