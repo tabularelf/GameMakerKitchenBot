1ce99c8781d1dc608ec0974aeaa7cc3883cff9f1
tabularelf <tabularelf@gmail.com>
2025-06-01 13:59:59 +1000

    Added resource filtering arguments

--- commands/resource.js
+++ commands/resource.js
@@ -12,8 +12,37 @@ module.exports = {
 			return message
 				.setName("resource")
 				.setDescription("The resource you'd like to recommend")
-				.setRequired(true)
+				.setRequired(false)
 				.setAutocomplete(true)
+		})
+		.addStringOption(author => {
+			return author.setName("author")
+				.setDescription("The author/s you wish to look up")
+				.setRequired(false)
+				.setAutocomplete(true)
+		})
+		.addStringOption(type => {
+			return type.setName("type")
+			.setDescription("The type of resource you wish to look up")
+			.setRequired(false)
+			.setChoices([
+				{name: 'plugin', value: 'plugin'},
+				{name: 'tutorial', value: 'tutorial'},
+				{name: 'library', value: 'library'},
+				{name: 'snippet', value: 'snippet'},
+				{name: 'tool', value: 'tool'},
+			])
+		})
+		.addBooleanOption(paid => {
+			return paid.setName("paid")
+			.setDescription("Whether this resource is paid or not")
+			.setRequired(false)
+		})
+		.addStringOption(tag => {
+			return tag.setName("tag")
+			.setDescription("The tag/s of a particular resource")
+			.setRequired(false)
+			.setAutocomplete(true)
 		})
 		.setIntegrationTypes([
         	ApplicationIntegrationType.GuildInstall, // Allows installation to servers (value: 0)
@@ -24,30 +53,137 @@ module.exports = {
     	    InteractionContextType.BotDM,           // Usable in DMs with the bot (value: 1)
     	    InteractionContextType.PrivateChannel   // Usable in Group DMs & other DMs if user-installed (value: 2)
     	]),
-	async autocomplete(interaction) {
-		const focusedValue  = interaction.options.getFocused().toLowerCase();
-
-
+	async autocomplete(interaction) {	
+		const focusedValue  = interaction.options.getFocused(true);
+		const inputValue = focusedValue.value.toLowerCase();
+		
+		//if (focusedValue.name == 'type') {
+		//	const output = [
+		//		
+		//	].sort((a, b) => a.name - b.name);
+		//	return await interaction.respond(output);
+		//}
+		
 		const output = [];
 		let data = fs.readFileSync(searchFile, 'utf8');
 		let obj = JSON.parse(data);
 		let count = 0;
-		for(key in obj) {
+
+		if (focusedValue.name == 'author') {
+			obj = obj.filter((value, index, array) => array.indexOf(value) === index);
+			let authors = [];
+			for(let elm of obj) {
+				for (let author of elm.author) {
+					authors.push(author.toLowerCase());
+				}
+			}
+			authors = authors.filter((value, index, array) => array.indexOf(value) === index);
+			for (let author of authors) {
+				if (author.startsWith(inputValue) && !output.includes(author)) {
+					output.push({name: author, value: author});
+					++count;
+				} else if ((inputValue == '') && !output.includes(author)) {
+					output.push({name: author, value: author});
+					++count;
+				} 
+				if (count > 24) break;
+			}
+			return await interaction.respond(output);
+		}
+
+		if (focusedValue.name == 'tag') {
+			obj = obj.filter((value, index, array) => array.indexOf(value) === index);
+			let tags = [];
+			for(let elm of obj) {
+				for (let tag of elm.tags) {
+					tags.push(tag.toLowerCase());
+				}
+			}
+
+			tags = tags.filter((value, index, array) => array.indexOf(value) === index);
+			for (let tag of tags) {
+				if (tag.includes(inputValue) && !output.includes(tag)) {
+					output.push({name: tag, value: tag});
+					++count;
+				} else if ((inputValue == '') && !output.includes(tag)) {
+					output.push({name: tag, value: tag});
+					++count;
+				} 
+				if (count > 24) break;
+			}
+			return await interaction.respond(output);
+		}
+
+		var author = interaction.options.getString('author');
+		var tag = interaction.options.getString('tag');
+		var type = interaction.options.getString('type');
+		var paid = interaction.options.getBoolean('paid');
+		if (author !== null || tag !== null || type !== null || paid !== null) {
+			if (author !== null) {
+				let distinct = [];
+				for(let elm of obj) {
+					if ((author ?? undefined) != undefined) {
+						if (elm.author.join(',').toLowerCase().split(',').includes(author)) {
+							distinct.push(elm);
+						}
+					}
+				}
+				obj = distinct;
+			}
+			
+			if (tag !== null) {
+				let distinct = [];
+				for(let elm of obj) {
+					if (elm.tags.includes(tag)) {
+						distinct.push(elm);
+					}
+				}
+				obj = distinct;
+			}
+
+			if (type !== null) {
+				let distinct = [];
+				for(let elm of obj) {
+					if (elm.type == type) {
+						distinct.push(elm);
+					}
+				}
+				obj = distinct;
+			}
+
+			if (paid !== null) {
+				let distinct = [];
+				for(let elm of obj) {
+					if (elm.paid == paid) {
+						distinct.push(elm);
+					}
+				}
+				obj = distinct;
+			}
+		}
+		
+		for(let key in obj) {
 			var element = obj[key];
-			if (`${element.label} - ${element.author.join(' ')}`.toLowerCase().startsWith(focusedValue) || 
-				`${element.title} - ${element.author.join(' ')}`.toLowerCase().startsWith(focusedValue) ||
-				element.authorsSafe.includes(focusedValue) ||
-				element.tags.includes(focusedValue)) {
+
+			if (`${element.label} - ${element.author.join(' ')}`.toLowerCase().startsWith(inputValue) || 
+				`${element.title} - ${element.author.join(' ')}`.toLowerCase().startsWith(inputValue) ||
+				element.author.join(' ').toLowerCase().includes(inputValue) ||
+				element.tags.includes(inputValue) ||
+				`${element.title}`.toLowerCase().startsWith(inputValue)) {
 				output.push({name: `${element.label} - ${element.author.join(', ')}`, value: element.title});
 				if (++count > 24) break;
 			} 
 		}
-		await interaction.respond(output);
+		return await interaction.respond(output);
 	},
 	async execute(interaction) {
 
 		var msg = interaction.options.getString("resource") ?? "";
 		msg = msg.toLowerCase();
+
+		if (msg == "") {
+			return await interaction.reply({ content: `Resource argument was not fulfilled. Please fill in a valid argument!`, ephemeral: true })
+		}
 
 		try {
 			if (!fs.existsSync(folderName)) {
